cc7c76e00021eb2fb52c7e81d7267df934b5efe2
benogit <benporter95@protonmail.com>
2021-03-24 15:48:10 +1000

    Refined fetch code and added working date input

--- script.js
+++ script.js
@@ -1,48 +1,50 @@
 const nasaPUrl='https://api.nasa.gov/planetary/apod?api_key=zoFCvvLGj3VZwQNMnXplAjxXwPJc8r7ApprYSxKK';
 
 document.addEventListener('DOMContentLoaded', function() {
-    displayPotd(), fetchmarsUrl()
+    displayPotd()
+
 })
 
 function displayPotd(){
-    fetchpotddesc();
-    fetchpotddate();
-    fetchpotdtitle();
-    fetchpotd();
-
-    function fetchpotddesc(){
-        fetch(nasaPUrl)
-            .then (response => response.json())
-            .then (data => document.getElementById("potd-desc").innerText=data.explanation)
-        };    
-
-    function fetchpotdtitle(){
-        fetch(nasaPUrl)
-            .then (response => response.json())
-            .then (data => document.getElementById("potd-title").innerText=data.title)
-        };
-
-    function fetchpotddate(){
-        fetch(nasaPUrl)
-            .then (response => response.json())
-            .then (data => document.getElementById("potd-date").innerText=data.date)      
-        };
-
-    function fetchpotd(){
-        fetch(nasaPUrl)
-         .then (response => response.json())
-         .then (data => document.getElementById("potdimage").src = data.url)       
-        };
-}
-
-const marsUrl= 'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=zoFCvvLGj3VZwQNMnXplAjxXwPJc8r7ApprYSxKK'
-
-function fetchmarsUrl(){
-    fetch(marsUrl)
+    fetch(nasaPUrl)
         .then (response => response.json())
-        .then (data => console.log(data))
-    }; 
+        .then (data => {
+            document.getElementById("potd-desc").innerText=data.explanation;
+            document.getElementById("potd-title").innerText=data.title;
+            document.getElementById("potd-date").innerText=data.date;
+            document.getElementById("potdimage").src=data.url;
+        })
+}   
 
-function displayMars(){
-    
-}
+formsubmit.addEventListener('click', function(event){
+    event.preventDefault()
+    changeDate()
+});   
+
+function changeDate(input){
+    fetch(nasaPUrl+'&date='+input.value)
+        .then (response => response.json())
+        .then (data => {
+            document.getElementById("potd-desc").innerText=data.explanation;
+            document.getElementById("potd-title").innerText=data.title;
+            document.getElementById("potd-date").innerText=data.date;
+            document.getElementById("potdimage").src=data.url;
+        })
+}  
+
+// const marsUrl= 'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=zoFCvvLGj3VZwQNMnXplAjxXwPJc8r7ApprYSxKK'
+
+// function fetchmarsUrl(){
+//     fetch(marsUrl)
+//         .then (response => response.json())
+//         .then (data => console.log(data))
+//     }; 
+
+// function displaymarsData(){
+//     displaymarsHeading();
+
+//     function displaymarsHeading(){
+//         document.getElementById("APOTD").innerText=("Mars Rover Photos")
+        
+//     }
+// }
