58e0a82a7f07fde767eebfe8e35c0e0d04c1c844
benogit <benporter95@protonmail.com>
2021-03-29 19:39:42 +1000

    Added Working Mars Rover Content

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const nasaPUrl='https://api.nasa.gov/planetary/apod?api_key=zoFCvvLGj3VZwQNMnXpl
 document.addEventListener('DOMContentLoaded', function() {
     displayPotd()
 
-})
+});
 
 function displayPotd(){
     fetch(nasaPUrl)
@@ -13,8 +13,9 @@ function displayPotd(){
             document.getElementById("potd-title").innerText=data.title;
             document.getElementById("potd-date").innerText=data.date;
             document.getElementById("potdimage").src=data.url;
+        
         })
-}   
+};  
 
 formsubmit.addEventListener('click', function(event){
     event.preventDefault()
@@ -32,19 +33,33 @@ function changeDate(input){
         })
 }  
 
-// const marsUrl= 'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=zoFCvvLGj3VZwQNMnXplAjxXwPJc8r7ApprYSxKK'
+const marsUrl= 'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=zoFCvvLGj3VZwQNMnXplAjxXwPJc8r7ApprYSxKK'
 
-// function fetchmarsUrl(){
-//     fetch(marsUrl)
-//         .then (response => response.json())
-//         .then (data => console.log(data))
-//     }; 
-
-// function displaymarsData(){
-//     displaymarsHeading();
-
-//     function displaymarsHeading(){
-//         document.getElementById("APOTD").innerText=("Mars Rover Photos")
+let index= 0
+ 
+function displaymarsData(){
+    fetch(marsUrl)
+    .then (response => response.json())
+    .then (data => {
+        console.log(data);
+        document.getElementById("potd-date").style.backgroundColor="red";document.getElementById("potd-title").style.backgroundColor="red";document.querySelector("#potdh").style.backgroundColor="red";document.getElementById("APOTD").style.backgroundColor="red";document.querySelector("div.potd-title").style.backgroundColor="red";document.querySelector("div.potd-date").style.backgroundColor="red";document.querySelector("h2").style.backgroundColor="red";document.getElementById("POTD").style.color="red";document.getElementById("MRP").style.color="red";document.getElementById("dateform").style.visibility="hidden";document.getElementById("prev").style.visibility="visible";document.getElementById("next").style.visibility="visible";
+        document.getElementById("potd-date").innerText="Date of Photo: "+data.photos[index].earth_date;
+        document.getElementById("APOTD").innerText=("Mars Rover Photos");
+        document.getElementById("potd-desc").innerHTML=data.photos[index].camera.name+":"+" "+data.photos[index].camera.full_name+"<br>"+"Launch Date:"+" "+data.photos[index].rover.launch_date+"<br>"+"Landing Date:"+" "+data.photos[index].rover.landing_date;
+        document.getElementById("potd-title").innerText=data.photos[index].rover.name+(" Rover");
+        document.getElementById("potdimage").src=data.photos[index].img_src;
         
-//     }
-// }
+    })
+} 
+
+function next(){
+    index++;
+    displaymarsData();
+    console.log(index);
+}
+
+function prev(){
+    index--;
+    displaymarsData();
+    console.log(index);
+}
